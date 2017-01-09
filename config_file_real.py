6a2afeb8ff34ba3f4f3b52f602a466dcd1c17818
xiuc <hang.lou@tqmall.com>
2017-01-09 20:22:34 +0800

    使用supervisor监控应用的发布脚本

--- config_file_real.py
+++ config_file_real.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # encoding=utf8
+"""
+可以添加多组服务器,用列表的形式
 
+每组有5个属性: 账户名称, 服务器ip, tomcat路径, supervisor管理的应用节点名称, 服务war包名
+"""
 # 测试环境
 host = [
-    ['tqmall.es', '10.160.16.177', '/data/es_server/tomcat', 'tomcat']
+    ['tqmall.es', '10.160.16.177', '/data/es_server/tomcat', 'tomcat', 'elasticsearch']
 ]
